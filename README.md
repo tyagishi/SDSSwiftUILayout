e8ec2e13a7506783b7518f722cf425f859bff228
Tomoaki Yagishita <tyagishi@gmail.com>
2023-01-07 19:54:35 +0900

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# SDSSwiftUILayout
+
+A description of this package.
---
+++ Sources/SDSSwiftUILayout/SDSSwiftUILayout.swift
@@ -0,0 +1,6 @@
+public struct SDSSwiftUILayout {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
